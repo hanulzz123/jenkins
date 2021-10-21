f1deb3797abb2974a5c07dde5393fbce929fa6d7
hanulzz123 <hanulzz123@gmail.com>
2021-10-21 15:45:58 +0900

    http 6

--- http.sh
+++ http.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo rm -rf /web
 sudo mkdir /web
-sudo yum install -y gcc gcc-c++ wget
+sudo yum install -y expat-devel gcc gcc-c++ wget
 sudo wget -P /web https://downloads.apache.org/apr/apr-1.7.0.tar.gz
 sudo wget -P /web https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
 sudo wget -P /web https://downloads.apache.org/httpd/httpd-2.4.51.tar.gz
